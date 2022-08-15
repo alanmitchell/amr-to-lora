3d0cf6eaa36c5b7dd9e0f59c009a40e0a75de07e
Alan Mitchell <tabb99@gmail.com>
2022-08-15 10:37:35 -0800

    Added an install script.

---
+++ install.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+# Run this script from your Home directory.  Do not plug in the RTL-SDR dongle
+# until the script is complete.
+
+# Most of this script is copied from 
+# https://gist.github.com/n8acl/dab196ce30e1e2139727cb7f76d300d6#file-rtlsdr-install-sh
+
+echo "-------------------------------------------"
+echo "-- Installing needed packages from repos --"
+echo "-------------------------------------------"
+sudo apt-get install -y git cmake libffi-dev libssl-dev build-essential libusb-1.0-0-dev pkg-config golang
+
+echo "------------------------------------------------"
+echo "-- Cloning rtl-sdr driver files from osmocom  --"
+echo "------------------------------------------------"
+
+sudo git clone git://git.osmocom.org/rtl-sdr.git
+
+echo "------------------------------------------------"
+echo "-- Building and Installing drivers            --"
+echo "------------------------------------------------"
+
+cd rtl-sdr
+sudo mkdir build
+cd build
+sudo cmake ../ -DINSTALL_UDEV_RULES=ON
+sudo make
+sudo make install
+sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
+sudo ldconfig
+
+echo "------------------------------------------------"
+echo "-- Blacklisting Certain RTL-SDR drivers       --"
+echo "------------------------------------------------"
+
+sudo touch /etc/modprobe.d/blacklist-rtl.conf
+
+sudo echo "blacklist dvb_usb_rtl28xxu" >> /etc/modprobe.d/blacklist-rtl.conf
+sudo echo "blacklist rtl2832" >> /etc/modprobe.d/blacklist-rtl.conf
+sudo echo "blacklist rtl2830" >> /etc/modprobe.d/blacklist-rtl.conf
+
+echo "------------------------------------------------"
+echo "-- Installing RTLAMR Meter Reading script     --"
+echo "------------------------------------------------"
+go get github.com/bemasher/rtlamr
+
+echo "------------------------------------------------"
+echo "-- Install the AMR to LoRa script          --"
+echo "------------------------------------------------"
+git clone https://github.com/alanmitchell/amr-to-lora.git
+
+
+echo "------------------------------------------------"
+echo "-- Now Reboot System and plug in RTL-SDR.     --"
+echo "-- See the amr-to-lora/README.md file for     --"
+echo "-- further configuration instructions.        --"
+echo "------------------------------------------------"
