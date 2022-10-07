0a957416ceec6191e88352c570d344a23d57b47c
Alan Mitchell <tabb99@gmail.com>
2022-10-06 20:00:09 -0800

    Added delay after reboot before starting script.

--- pi_prep.sh
+++ pi_prep.sh
@@ -6,6 +6,6 @@ sudo mkdir /boot/amr
 sudo cp settings_example.py /boot/amr/settings.py
 ln -s /boot/amr/settings.py settings.py
 crontab -l > cron_bak
-printf "\n@reboot /home/pi/amr-to-lora/env/bin/python /home/pi/amr-to-lora/main.py \n0 4 * * * sudo /sbin/shutdown -r +1" >> cron_bak
+printf "\n@reboot sleep 15 && /home/pi/amr-to-lora/env/bin/python /home/pi/amr-to-lora/main.py \n0 4 * * * sudo /sbin/shutdown -r +1\n" >> cron_bak
 crontab cron_bak
 rm cron_bak
