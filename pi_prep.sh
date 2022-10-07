60102050a8be3d715264a4563bb6f2e71eda874e
Alan Mitchell <tabb99@gmail.com>
2022-10-06 19:32:26 -0800

    Added script for implementing Raspberry Pi changes.

---
+++ pi_prep.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# Additional steps useful for preparing amr-to-lora on a Raspberry Pi.
+# Change into the amr-to-lora directory before running this script.
+# Run install.sh first.
+sudo mkdir /boot/amr
+sudo cp settings_example.py /boot/amr/settings.py
+ln -s /boot/amr/settings.py settings.py
+crontab -l > cron_bak
+printf "\n@reboot /home/pi/amr-to-lora/env/bin/python /home/pi/amr-to-lora/main.py \n0 4 * * * sudo /sbin/shutdown -r +1" >> cron_bak
+crontab cron_bak
+rm cron_bak
